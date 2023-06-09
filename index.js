8e4f2d5e5d12b3cfa8e49be265962bebbbc81502
Robby Lewis <Robbysim37@gmail.com>
2023-06-09 19:24:16 -0400

    added express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const test = "test"
+const express = require("express")
+const server = express()
+
+server.get(`/`,(req,res) => {
+    res.send("Testing the backend")
+})
+
+server.get(`/test`,(req,res) => {
+    res.send("This is different")
+})
+
+server.listen(8000, () => {
+    console.log("server running on port: 8000")
+})
