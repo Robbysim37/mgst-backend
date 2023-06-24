3040563ee03dd13872f66533af0a8115a3a12466
Robby Lewis <Robbysim37@gmail.com>
2023-06-24 14:40:28 -0400

    started on schedule

---
+++ scheduleSchema/schedule.js
@@ -0,0 +1,28 @@
+
+// schedule contains 4 "year" arrays
+// Each year contains 3 "trimester" arrays
+// each trimester contains 3 "class" objects
+
+
+
+const createSchedule = () => {
+    [
+        [
+            [{},{},{}],[{},{},{}],[{},{},{}]
+        ],
+        [
+            [{},{},{}],[{},{},{}],[{},{},{}]
+        ],
+        [
+            [{},{},{}],[{},{},{}],[{},{},{}]
+        ],
+        [
+            [{},{},{}],[{},{},{}],[{},{},{}]
+        ]
+    ]
+}
+
+
+module.exports = {
+
+}
