7e987baf5e41db2115940678a4168fc77e52e7f5
Robby Lewis <Robbysim37@gmail.com>
2023-06-24 20:47:59 -0400

    Schedule schema update, class construction

--- scheduleSchema/schedule.js
+++ scheduleSchema/schedule.js
@@ -4,12 +4,33 @@
 // each trimester contains 3 "class" objects
 
 
+const createCourse = (className,creditType,completed,notes) => {
+    const course = 
+    {
+        name: className,
+        creditType: creditType,
+        completed: completed,
+        notes: notes,
+        creditAmount: .5
+    }
+    return course
+}
+
+
 
 const createSchedule = () => {
     [
         [
-            [{},{},{}],
-            [{},{},{}],
+            [
+            createCourse("Algebra 1A","Math",false,""),
+            createCourse("Freshman English A","English",false,""),
+            createCourse("World History","History",false,"")
+            ],
+            [
+            createCourse("Algebra 2B","Math",false,""),
+            createCourse("Freshman English B","English",false,""),
+            createCourse("Algebra 2B","Math",false,"")
+            ],
             [{},{},{}]
         ],
         [
